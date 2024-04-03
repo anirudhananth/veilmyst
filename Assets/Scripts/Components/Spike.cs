d30da87f2359afbd7e1e2e8467fe454cdc723563
Zixuan James Li <p359101898@gmail.com>
2024-04-03 10:30:40 -0400

    Add support to spikes.
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

---
+++ Assets/Scripts/Components/Spike.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(LethalCollision))]
+public class Spike : MonoBehaviour
+{
+    private LethalCollision collision;
+
+    [Tooltip("Only objects moving against this direction can be killed by the spike")]
+    private Vector2 direction = Vector2.up;
+
+    private void Start()
+    {
+        collision = GetComponent<LethalCollision>();
+        direction.Normalize();
+
+        void handler(LethalCollision self, Destructible destructible)
+        {
+            Rigidbody2D rb = destructible.GetComponent<Rigidbody2D>();
+
+            // Only kill if the movement is in an opposite direction
+            Debug.Log($"Dot product {rb.velocity.normalized} · {direction} = {Vector2.Dot(rb.velocity.normalized, direction)}");
+            if (rb == null || Vector2.Dot(rb.velocity.normalized, direction) < 0.001)
+            {
+                destructible.Die(self.gameObject);
+            }
+        }
+
+        collision.OnCollideDestructible = handler;
+    }
+}
