e519fa2e8905da11fd65b904751f7fa632181780
Rikaero <ritvikdeepika@gmail.com>
2024-04-08 16:03:49 -0400

    Modified scrpit to try and fix the cam bug - needs testing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int DeathCount;
-
+    // Camera related vars
     Camera MainCam;
     public Vector3 CPPos;
     public Vector3 CamPos;
@@ -13,26 +12,35 @@ public class GameManager : MonoBehaviour
     private float distance;
     private float distanceCovered;
 
+    // Player-related vars
     public GameObject player;
+    public Movement playMovScript;
+
+    // Misc
+    public int DeathCount;
 
     // Start is called before the first frame update
     void Start()
     {
         MainCam = Camera.main;
+        
+        player = GameObject.FindGameObjectWithTag("Player");
+        playMovScript = player.GetComponent<Movement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //MainCam.transform.position = CamPos;
-        
+        playMovScript.spawnLocation = CPPos;
     }
 
     public IEnumerator move()
     {
         speed = 0;
         distance = Vector3.Distance(MainCam.transform.position, CamPos);
-        Time.timeScale = 0;
+
+        //Time.timeScale = 0;
+        playMovScript.canDash = false;
         while (speed < 1)
         {
             MainCam.transform.position = Vector3.Lerp(MainCam.transform.position, CamPos, speed);
