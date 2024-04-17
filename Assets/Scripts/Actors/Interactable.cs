c4ec2aa417b4b860484cd06b3c3f590a3c13dbc5
Zixuan James Li <p359101898@gmail.com>
2024-04-16 21:28:56 -0400

    Add interactable floating effect.
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- Assets/Scripts/Actors/Interactable.cs
+++ Assets/Scripts/Actors/Interactable.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    private static readonly float FloatMaxOffset = 0.1f;
 
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 initPos;
+    private float floatRad = 0;
+    [Tooltip("The time it takes for the interactable to float up and down in seconds")]
+    private readonly float floatCycleInterval = 2f;
+    private float floatSpeedRad;
+
+    private void Start()
     {
-        
+        initPos = transform.position;
+        floatSpeedRad = 2 * Mathf.PI / floatCycleInterval;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        floatRad += floatSpeedRad * Time.deltaTime;
+        Vector3 newPos = initPos;
+        newPos.y += FloatMaxOffset * Mathf.Sin(floatRad);
+        transform.position = newPos;
     }
 }
