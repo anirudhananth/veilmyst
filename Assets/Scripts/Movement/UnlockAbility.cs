669a8ebe55508ac5346624f786219b255753c0a5
Rikaero <ritvikdeepika@gmail.com>
2024-04-24 16:12:05 -0400

    small change

--- Assets/Scripts/Movement/UnlockAbility.cs
+++ Assets/Scripts/Movement/UnlockAbility.cs
@@ -31,7 +31,7 @@ public class UnlockAbility : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
