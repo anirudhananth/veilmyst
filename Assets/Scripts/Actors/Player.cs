98ebcaba232ddbb4ad8f9ae0b8069eed021476c4
Zixuan James Li <p359101898@gmail.com>
2024-04-24 18:00:51 -0400

    Fix user respawn bug.
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- Assets/Scripts/Actors/Player.cs
+++ Assets/Scripts/Actors/Player.cs
@@ -37,7 +37,6 @@ public class Player : Actor
             player.animator.SetBool("isDead", false);
             player.rb.isKinematic = false;
             player.staminaBar.gameObject.SetActive(player.staminaBar.isvisible);
-            player.staminaBar.stamina.currentstamina = player.staminaBar.stamina.maxstamina;
             yield return new WaitForSeconds(0.2f);
             player.input.ActivateInput();
         }
