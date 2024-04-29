5c0cf0fea9c87918bfd8ad462838755df80135bd
Zixuan James Li <p359101898@gmail.com>
2024-04-29 01:54:35 -0400

    Implement runtime display of rebound keys in the tutorial.
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- Assets/Scripts/Movement/UnlockAbility.cs
+++ Assets/Scripts/Movement/UnlockAbility.cs
@@ -9,40 +9,42 @@ public class UnlockAbility : MonoBehaviour
 {
     // Player Input vars
     [Header("Player Input Vars")]
-    public string inputName;
+    public InputActionReference InputActionRef;
     public GameObject Player;
-    PlayerInput playerInput;
 
     // UI vars
     [Header("UI vars")]
     public TMP_Text uiText;
     public string Instruction;
+
     private bool helped = false;
     private Animator animator;
+    private string activeInstruction;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerInput = Player.GetComponent<PlayerInput>();
-        playerInput.actions.FindAction(inputName).Disable();
+        activeInstruction = Instruction;
         uiText.GetComponent<TextMeshProUGUI>().text = "";
         animator = uiText.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Enable the action if not before
         if (!helped)
         {
             helped = true;
-            playerInput.actions.FindAction(inputName).Enable();
+            if (InputActionRef != null) InputActionRef.action.Enable();
         }
-        uiText.text = Instruction;
+
+        // Replace the placeholder text with the key binding display string
+        if (InputActionRef != null)
+        {
+            string inputName = InputActionRef.action.GetBindingDisplayString(0);
+            activeInstruction = Instruction.Replace("KEY", inputName);
+        }
+        uiText.text = activeInstruction;
         animator.SetBool("show", true);
     }
 
@@ -50,9 +52,13 @@ public class UnlockAbility : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            if (uiText.text == Instruction)
+            if (uiText.text == activeInstruction)
             {
-                animator.SetBool("show", false);
+                // This check accounts for the case when the player goes to the
+                // next scene.
+                if (animator != null) {
+                    animator.SetBool("show", false);
+                }
             }
         }
     }
