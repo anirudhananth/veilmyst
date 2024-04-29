351eeb238624c2608cabf2ad76b7932c87bb0c73
Rikaero <ritvikdeepika@gmail.com>
2024-04-29 00:56:23 -0400

    original

---
+++ Assets/Scripts/PhaseManager.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PhaseManager : MonoBehaviour
+{
+    [Header("Phase Objects")]
+    public bool phase;
+    public GameObject phaseA;
+    public GameObject phaseB;
+
+    [Header("Timing Variables")]
+    private float timer = 0;
+    public float cooldown; // The amount by which the timer increases
+    public TMP_Text timerUI;
+    public float timeLeft;
+
+    [Header("Phase Type")]
+    public bool dashType;
+    public bool timeType;
+
+    [Header("Dash Type")]
+    public GameObject Player;
+    Movement playerMovement;
+    PlayerInput playerInput;
+
+    [Header("Time Type")]
+    private float shifTimer = 0; // If it is in timer mode then this is when it will change phase
+    public float shiftCD; // The amount by which shiftimer changes
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Player
+        playerMovement = Player.GetComponent<Movement>();
+        playerInput = Player.GetComponent<PlayerInput>();
+
+        // UI
+        timerUI.GetComponent<TextMeshProUGUI>().text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Phase Change basics
+        if (phase)
+        {
+            phaseA.SetActive(true);
+            phaseB.SetActive(false);
+        }
+        else
+        {
+            phaseA.SetActive(false);
+            phaseB.SetActive(true);
+        }
+
+        // If dashType
+        if (timer > Time.time)
+        {
+            timeLeft = timer - Time.time;
+            timerUI.text = timeLeft.ToString();
+            if (timeType)
+            {
+                TimedChanger();
+            }
+            if (dashType)
+            {
+                DashChanger();
+            }
+        }
+
+        // Button for toggle
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            phase = !phase;
+        }
+    }
+
+    public void PhaseChanger() // Called by button
+    {
+        timer = Time.time + cooldown;
+        // phase = !phase;
+        if (!timeType && !dashType)
+        {
+            phase = !phase;
+        }
+    }
+
+    void DashChanger() // Change when dashing
+    {
+        if (playerInput.actions["Dash"].triggered && playerMovement.canDash)
+        {
+            phase = !phase;
+        }
+    }
+
+    void TimedChanger() // Phase changes after some seconds
+    {
+        if(shifTimer < Time.time)
+        {
+            phase = !phase;
+            shifTimer = Time.time + shiftCD;
+        }
+    }
+}
