c1e1b697f9be41d7ba8c2c12255b4949ace8bf0b
Zixuan James Li <p359101898@gmail.com>
2024-05-06 02:20:46 -0400

    Fix checkpoint animation issue.
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -57,8 +57,8 @@ public class CheckPoint : MonoBehaviour
                     if(GM.ActiveSpawnPoint)
                     {
                         GM.ActiveSpawnPoint?.Deactivate();
-                        Activate();
                     }
+                    Activate();
                     
                 }
                 GM.SetSpawn(this);
